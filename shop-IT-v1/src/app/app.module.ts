f9c13bc6c2c59edea8e7f3582f60e887a5af57f5
Nagabhushanam <nagabhushanam.n@revature.com>
2023-03-14 12:25:18 +0530

    cart badge

--- shop-IT-v1/src/app/app.module.ts
+++ shop-IT-v1/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductComponent } from './product/product.component';
 import { ReviewComponent } from './review/review.component';
+import { CartBadgeComponent } from './cart-badge/cart-badge.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { ReviewComponent } from './review/review.component';
     HeaderComponent,
     ProductListComponent,
     ProductComponent,
-    ReviewComponent
+    ReviewComponent,
+    CartBadgeComponent
   ],
   imports: [
     BrowserModule
